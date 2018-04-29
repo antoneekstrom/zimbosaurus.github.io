e73ddf511141ab2ca06f74ef07c96dc1b23fabe5
Anton Ekström <31110431+zimbosaurus@users.noreply.github.com>
2018-04-29 15:48:06 +0200

    dynamic content with xml

--- js/xml.js
+++ js/xml.js
@@ -1,4 +1,4 @@
-var file = "../data/content.xml";
+var file = "../data/index.xml";
 var parser, xmldoc, xhttp, xmltext;
 
 function getXml(xml) {
@@ -8,7 +8,6 @@ function getXml(xml) {
 
 function findTag(tagname, n) {
     var text = xmldoc.getElementsByTagName(tagname)[n].firstChild.nodeValue;
-    console.log(text);
 }
 
 function getXmlElement(tagname, n) {
@@ -30,11 +29,99 @@ $(document).ready(function() {
     xhttp.send();
     
     $("#test").click(function () {
+        
+        //find all content elements in .xml
         for (var i = 0; i < xmldoc.getElementsByTagName("content").length; i++) {
-            for (var k = 0; k < xmldoc.getElementsByTagName("title").length; k++) {
-                var name = xmldoc.getElementsByTagName("title")[k].firstChild.nodeValue;
-                console.log(name);
+            
+            //elements from .xml
+            var xcontent = xmldoc.getElementsByTagName("content")[i];
+            var xelement;
+            
+            //content elements
+            var content = $("<div class='content'></div>");
+            var h2 = $("<h2></h2>");
+            var p = $("<p></p>");
+            var img = $("<img>");
+            
+            //find content tags in .xml file
+            for (var k = 0; k < xcontent.childElementCount; k++) {
+                
+                //finds content child elements
+                xelement = xcontent.children[k];
+                
+                //finds title element if there is one
+                if (xelement.nodeName == "title") {
+                    
+                    title = xelement.firstChild.nodeValue;
+                    if (xelement.hasAttribute("id")) {
+                        id = xelement.getAttribute("id");
+                    } else {
+                        id = title;
+                    }
+                    
+                    $(h2).append(title);
+                    $(h2).attr("id", id);
+                    $(content).append(h2);
+                    
+                    //add title to sidebar
+                    var navdiv = $("<div></div>");
+                    var navanchor = $("<a></a>");
+                    var navli = $("<li></li>");
+                    $(navanchor).attr("href", "#" + id);
+                    $(navli).append(id);
+                    $(navanchor).append(navli);
+                    $(navdiv).append(navanchor);
+                    $("#sidebar").append(navdiv);
+                    
+                }
+                
+                //finds body element if there is one
+                if (xelement.nodeName == "body") {
+                    
+                    body = xelement.firstChild.nodeValue;
+                    $(p).append(body);
+                    $(content).append(p);
+                    
+                }
+                
+                //finds image element if there is one
+                if (xelement.nodeName == "image") {
+                    var src = xelement.getAttribute("src");
+                    $(img).attr("src", src);
+                    $(img).addClass("incontent");
+                    $(content).append(img);
+                }
+                
+                //finds link if there is one
+                if (xelement.nodeName == "link") {
+                    var div = $("<div></div>");
+                    var a = $("<a></a>");
+                    var li = $("<li></li>");
+                    var text = xelement.firstChild.nodeValue;
+                    
+                    //adds link if there is one
+                    if (xelement.hasAttribute("href")) {
+                        var destination = xelement.getAttribute("href");
+                        $(a).attr("href", destination);
+                        $(a).attr("target", "_blank");
+                    }
+                    
+                    $(li).append(text);
+                    $(a).append(li);
+                    $(div).append(a);
+                    $(content).append(div);
+                    
+                }
+                
+                $("#container").append(content);
             }
         }
     });
 });
+
+
+
+
+
+
+
